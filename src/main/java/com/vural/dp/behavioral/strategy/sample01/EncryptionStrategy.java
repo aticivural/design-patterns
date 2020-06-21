be0311be2c440918bfb2b05f1f60d82e880a880c
traticiv <vural.atici@sony.com>
2020-06-22 00:30:26 +0300

    add strategy pattern

---
+++ src/main/java/com/vural/dp/behavioral/strategy/sample01/EncryptionStrategy.java
@@ -0,0 +1,5 @@
+package com.vural.dp.behavioral.strategy.sample01;
+
+public interface EncryptionStrategy {
+    void encryptData(String plainText);
+}
---
+++ src/main/java/com/vural/dp/behavioral/strategy/sample01/Encryptor.java
@@ -0,0 +1,22 @@
+package com.vural.dp.behavioral.strategy.sample01;
+
+public class Encryptor {
+    private EncryptionStrategy strategy;
+    private String plainText;
+
+    public Encryptor(EncryptionStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void encrypt() {
+        strategy.encryptData(plainText);
+    }
+
+    public String getPlainText() {
+        return plainText;
+    }
+
+    public void setPlainText(String plainText) {
+        this.plainText = plainText;
+    }
+}
